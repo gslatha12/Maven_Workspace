64781795bfa00a2823358ee91c2c2e468d8c7afa
SRINIVAS <SRINIVAS@DESKTOP-T998RO5>
2023-01-09 09:03:57 +0530

    My first update

---
+++ Maven_Project/src/test/java/p1/Class1.java
@@ -0,0 +1,12 @@
+package p1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Class1 {
+  @Test
+  public void test1() 
+  {
+	  Reporter.log("tc1 executed");
+  }
+}
---
+++ Maven_Project/src/test/java/p2/Class2.java
@@ -0,0 +1,28 @@
+package p2;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Class2 {
+  @Test
+  public void test2() 
+  {
+	  Reporter.log("tc2 executed",true);
+	  
+  }
+  @Test
+  public void test3() 
+  {
+	  Reporter.log("tc3 executed",true);
+  }
+  @Test
+  public void test4() 
+  {
+	  Reporter.log("tc4 executed",true);
+  }
+  @Test
+  public void test5() 
+  {
+	  Reporter.log("tc5 executed",true);
+  }
+}
